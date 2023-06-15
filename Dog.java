a8be314264b6ebcc6e2beff43f7c8a73ffc841d0
Jonathan Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-06-14 21:56:58 -0230

    Created Dog Subclass
    Created the Dog subclass of Mammal

---
+++ Dog.java
@@ -0,0 +1,17 @@
+class Dog extends Mammal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void greets() {
+        System.out.println("Woof");
+    }
+
+    public void greets(Dog another) {
+        System.out.println("Wooof");
+    }
+
+    public String toString() {
+        return "Dog[" + super.toString() + "]";
+    }
+}
